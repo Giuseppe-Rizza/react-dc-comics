305a35d7cd657773d66e71a26c19d55078d2d6ad
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-02-13 22:26:26 +0100

    [feat] Add Navbar.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,15 @@
 // Importo il componente Logo.jsx
 import Logo from "../components/Logo.jsx";
 
+// Importo il componente Navbar.jsx
+import Navbar from "../components/Navbar.jsx";
+
 // Header.jsx
 export default function Header() {
     return (
         <header>
             <Logo />
-            <nav>
-                <ul>
-                    <li><a href="#">CHARACTERS</a></li>
-                    <li><a href="#" className="active">COMICS</a></li>
-                    <li><a href="#">MOVIES</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">GAMES</a></li>
-                    <li><a href="#">COLLECTIBLES</a></li>
-                    <li><a href="#">VIDEOS</a></li>
-                    <li><a href="#">FANS</a></li>
-                    <li><a href="#">NEWS</a></li>
-                    <li><a href="#">SHOP</a></li>
-                </ul>
-            </nav>
+            <Navbar />
         </header>
     );
 };
