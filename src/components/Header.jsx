49c7312af3b92e103dcf30108836f2b238a2ee12
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-02-16 10:41:32 +0100

    [feat] Add part two day two

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,28 @@ import Navbar from "../components/Navbar.jsx";
 
 // Header.jsx
 export default function Header() {
+
+    // Ricavo l'array di dati per la gestione dei link della Navbar
+    const links = [
+        { id: 1, name: 'CHARACTERS', url: '#', current: false },
+        { id: 2, name: 'COMICS', url: '#', current: true },
+        { id: 3, name: 'MOVIES', url: '#', current: false },
+        { id: 4, name: 'TV', url: '#', current: false },
+        { id: 5, name: 'GAMES', url: '#', current: false },
+        { id: 6, name: 'COLLECTIBLES', url: '#', current: false },
+        { id: 7, name: 'VIDEOS', url: '#', current: false },
+        { id: 8, name: 'FANS', url: '#', current: false },
+        { id: 9, name: 'NEWS', url: '#', current: false },
+        { id: 10, name: 'SHOP', url: '#', current: false },
+    ];
+
     return (
         <header>
             <Logo />
-            <Navbar />
+
+            {/* Il primo links è il nome della proprietà (props) che arriva dall'altra parte, il secondo links è il valore che gli stiamo passando (la costante dell'array che inizia a riga 11) e va messo tra le graffe perché è ricavato da una costante, quindi non gli passiamo direttamente un valore ma quello che la variabile links ci ritorna (se fosse semplicemente una stringa non ci vorrebbero le graffe)*/}
+            {/* N.B.: in altre parole, visto che i dati sono sul padre Header, devo passare questi dati in qualche modo al figlio Navbar e li passo dandogli un nome (linksProp) che ti ritrovi dalla'altra parte dagli attributi (links) */}
+            <Navbar linksProp={links} />
         </header>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 // Navbar.jsx
-export default function Navbar() {
+// Qui props sarà un oggetto che conterrà un'unica proprietà che si chiama linksProp (così l'ho chiamata dall'altra parte)
+export default function Navbar(props) {
+
+    // Siccome da questa parte mi ritorni l'oggetto props, a me interessa di quell'oggetto props cosa c'è come valore della proprità linksProp che quindi conterrà links (ossia l'array di oggetti) e quindi in Navbar.jsx posso utilizzarlo come links
+    const links = props.linksProp;
+    // Versione destrutturata
+    // const {linksProp} = props;
+
     return (
         <nav>
             <ul>
-                <li><a href="#">CHARACTERS</a></li>
+                {links.map((link) => (
+                    <li key={link.id}>
+                        <a href={link.url} className={link.current ? "active" : ""}>
+                            {link.name}
+                        </a>
+                    </li>
+                ))};
+                {/* <li><a href="#">CHARACTERS</a></li>
                 <li><a href="#" className="active">COMICS</a></li>
                 <li><a href="#">MOVIES</a></li>
                 <li><a href="#">TV</a></li>
@@ -12,7 +26,7 @@ export default function Navbar() {
                 <li><a href="#">VIDEOS</a></li>
                 <li><a href="#">FANS</a></li>
                 <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
+                <li><a href="#">SHOP</a></li> */}
             </ul>
         </nav>
     );
